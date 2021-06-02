09aa6a9d1bc2141261eef273a01922788ec97230
Cicero Junior <cicerojunior9999@gmail.com>
2021-06-02 19:10:37 -0300

    teste

--- php/teste.php
+++ php/teste.php
@@ -4,7 +4,7 @@ $username = "epiz_28780994";
 $password = "iu5ffU7PrheTg";
 
 try {
-  $conn = new PDO("mysql:host=$servername;dbname=teste charset=utf8", $username, $password);
+  $conn = new PDO("mysql:host=$servername;dbname=teste; charset=utf8", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   echo "Conexão Sucesso";
