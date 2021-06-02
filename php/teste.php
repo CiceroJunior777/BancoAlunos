b1c1412d0dd2e1ead64a9b418e84f101d0550b5c
Cicero Junior <cicerojunior9999@gmail.com>
2021-06-02 18:55:18 -0300

    banco conexao

---
+++ php/teste.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "sql111.epizy.com";
+$username = "epiz_28780994";
+$password = "iu5ffU7PrheTg";
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=teste charset=utf8", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Conexão Sucesso";
+} catch(PDOException $e) {
+  echo "Conexão Sem Sucesso: " . $e->getMessage();
+}
+?>
