d43d5d75ff8df504fdc1471ce07b27085669c883
Cicero Junior <cicerojunior9999@gmail.com>
2021-06-10 21:22:48 -0300

    estilizacao das tabelas

---
+++ php/banco.php
@@ -0,0 +1,30 @@
+<?php
+
+$servername = "sql111.epizy.com";
+$username = "epiz_28780994";
+$password = "iu5ffU7PrheTg";
+$dbname = "epiz_28780994_bancoalunos";
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $stmt = $conn->prepare("SELECT * FROM alunos");
+  $stmt->execute();
+
+
+  $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+  foreach($stmt->fetchAll()) as $k=>$v) {
+    echo '<tr>';
+    echo '<td>'.$v['Aluno'].'</td>';
+    echo '<td>'.$v['ID_Aluno'].'</td>';
+    echo '<td>'.$v['Disciplina'].'</td>';
+    echo '<td>'.$v['N1'].'</td>';
+    echo '<td>'.$v['N2'].'</td>';
+    echo'</tr>';
+  }
+} catch(PDOException $e) {
+  echo "Error: " . $e->getMessage();
+}
+$conn = null;
+echo "</table>";
+?>
